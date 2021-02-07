8d64d6a07d10a9f1fa601135f4d910470270ad1e
NataliaKurash <kurashnataliiamariia@gmail.com>
2021-02-07 14:46:24 +0100

    add node app

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require('path');
+const port = 3000;
+const functions =  require('./functions');
+const app = express();
+app.set("view engine","hbs");
+app.use('/assets', express.static(path.join(__dirname, "./assets")));
+app.use('/js',express.static(path.join(__dirname,"./js")));
+const fromAnotherFile = require("./functions")
+app.get('/', function (req, res) {
+res.render('index', {
+title: "Lekcja01",
+pageTitle: 'Lekcja01 o Nodzie',
+subTitle: functions.subTitle
+})
+})
+app.get("/about", function (req,res){
+    res.send("Strona o nas");
+})
+
+app.listen(port,(err)=>{
+    if (err) { return console.log(err)}
+    console.log(`Apka działa na porcie ${port}`)
+})
---
+++ functions.js
@@ -0,0 +1,11 @@
+const sayHello = () => { console.log("Hello World") }
+const add = (a, b) => {
+console.log(a + b)
+}
+const subTitle = 'WSB'; 
+const someTitle = "example example" //kod do dopisania
+module.exports = {
+sayHello,
+add,
+subTitle
+}
